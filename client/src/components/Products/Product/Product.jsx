8a10939d3c84cd7bcd8c231ac338f3a5565ec13f
Shibu4064 <hrithikmajumdershibu@gmail.com>
2023-06-01 10:46:41 +0600

    sumon-shibu

---
+++ client/src/components/Products/Product/Product.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Product.scss";
+
+const Product = ({ data, id }) => {
+    const navigate = useNavigate();
+    // console.log("image url",data?.image?.data[0]?.attributes?.url)
+    // console.log(data.img.data[0].attributes.url)
+    return (
+        <div
+            className="product-card"
+            onClick={() => navigate("/product/" + id)}
+        >
+            <div className="thumbnail">
+                <img
+                    src={
+                        process.env.REACT_APP_DEV_URL +
+                        data?.img?.data[0].attributes.url
+                    }
+                />
+            </div>
+            <div className="prod-details">
+                <span className="name">{data.title}</span>
+                <span className="price">&#8377;{data.price}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
